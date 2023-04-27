b71430ae4412ad2be4f84966caed66ed20469401
Noastud <noa.canonica@stud.edubs.ch>
2023-04-27 18:14:19 +0200

    tests

--- Details.php
+++ Details.php
@@ -9,101 +9,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Archive</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css?v=2">
 </head>
-<style>
-    .title5 {
-    text-align: center;
-    margin-top: 30px;
-  }
-  
-  .title5 h1 {
-
-font-size: 96px;
-font-weight: 500;
-line-height: 144px;
-letter-spacing: -0.022em;
-text-align: center;
-
-  }
-
-  .search-bar {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-}
-
-.search-bar input[type="text"] {
-    background-color: #FFFFFF;
-    width: 250%;
-    border-radius: 25px;
-    padding: 10px;
-    border: none;
-    outline: none;
-}
-
-
-.popup {
-    position: absolute;
-    top: 55px;
-    left: 0;
-    width: 100%;
-    max-height: 200px;
-    overflow-y: scroll;
-    background-color: #FFFFFF;
-    border: 1px solid #E5E5E5;
-    border-radius: 8px;
-    z-index: 1;
-    display: none;
-}
-
-.popup ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
-
-.popup ul li {
-    padding: 8px;
-    font-size: 18px;
-    color: #1F2041;
-    cursor: pointer;
-}
-
-.popup ul li:hover {
-    background-color: #F5F5F5;
-}
-
-.search-bar button {
-background-color: #fff;
-border: none;
-border-radius: 20px;
-width: 40px;
-height: 40px;
-position: absolute; 
-right: 0px; 
-margin-right: 45%;
-
-}
-
-.search-bar button:before {
-content: "\1F50E";
-font-size: 20px;
-color: #555;
-line-height: 40px;
-text-align: center;
-display: block;
-
-}
-    </style>
 
 <body>
 <header>
         <h1>Book Archive</h1>
 
-    <    <nav>
+       <nav>
             <button class="btn" ><a href="login.php"> Login</a></button>
         <hamburger-icon>
             <span></span>
@@ -118,8 +31,8 @@ display: block;
     </nav>
     </header>
    
-      <div class="title5">
-  <h1>Bookly</h1>
+      <div>
+  <h1 class="title5">Bookly</h1>
 </div>
     
 <div class="search-bar">
