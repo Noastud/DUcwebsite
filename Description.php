2b345c2c0447aace065efb7d8c22c1224fa5617b
Noastud <104830641+Noastud@users.noreply.github.com>
2023-04-28 21:40:13 +0200

    Descriptionfinal?

--- Description.php
+++ Description.php
@@ -32,71 +32,70 @@
 </header>   
 <div class="wrapper">
   <div class="box">
+  <?php
+    // startet die session
+    session_start();
 
-<?php
-// startet die session
-session_start();
+    // erstellt eine verbindung zur datenbank
+    $conn = mysqli_connect("localhost", "root", "", "books");
 
-// erstellt eine verbindung zur datenbank
-$conn = mysqli_connect("localhost", "root", "", "books");
+    // Check überprüft verbindung
+    if (!$conn) {
+      die("Verbindung fehlgeschlagen: " . mysqli_connect_error());
+    } else {
+       "Verbindung erfolgreich";
+    }
 
-// Check überprüft verbindung
-if (!$conn) {
-    die("Verbindung fehlgeschlagen: " . mysqli_connect_error());
-} else {
-     "Verbindung erfolgreich";
-}
+    if (isset($_GET['id'])) {
+      // GET die ID von einer anderen seite
+      $book_id = mysqli_real_escape_string($conn, $_GET['id']);
 
-if (isset($_GET['id'])) {
-    // GET die ID von einer anderen seite
-    $book_id = mysqli_real_escape_string($conn, $_GET['id']);
+      // SQL statement um die details des buches zu bekommen
+      $sql_book_details = "SELECT * 
+                           FROM buecher 
+                           WHERE id = '$book_id'";
+      // führt die abfrage aus
+      $result_book_details = $conn->query($sql_book_details);
 
-    // SQL statement um die details des buches zu bekommen
-    $sql_book_details = "SELECT * 
-                         FROM buecher 
-                         WHERE id = '$book_id'";
-// führt die abfrage aus
-    $result_book_details = $conn->query($sql_book_details);
-
-    // Check ob die abfrage funktioniert hat
-    if ($result_book_details) {
-      if (mysqli_num_rows($result_book_details) > 0) {
+      // Check ob die abfrage funktioniert hat
+      if ($result_book_details) {
+        if (mysqli_num_rows($result_book_details) > 0) {
           $book_details = $result_book_details->fetch_assoc();
           $random_cover = "https://picsum.photos/300/450?random=" . $book_details['id'];
-          
-          echo '<div class="book-details">';
-        
-          echo "<h2>" . $book_details['kurztitle'] . "</h2>";
-          echo "<p><strong>Titel:</strong> " . $book_details['title'] . "</p>";
-          echo '<div class="book-details-text-container">';
-          echo "<p><strong>Autor:</strong> " . $book_details['autor'] . "</p>";
-          echo "<p><strong>Verfasser:</strong> " . $book_details['verfasser'] . "</p>";
-          echo "<p><strong>Sprache:</strong> " . $book_details['sprache'] . "</p>";
-          echo "<p><strong>Zustand:</strong> " . $book_details['zustand'] . "</p>";
-          
-          if (isset($book_details['text'])) {
-              echo '<p><strong>Inhalt:</strong></p>';
-              echo '<p>' . $book_details['text'] . '</p>';
-          }
-          
-          echo '</div>';
-          echo '<div class="book-details-image" style="background-image: url(' . $random_cover . ')"></div>';
-          echo '</div>';
-      } else {
-          echo "Das Buch konnte nicht gefunden werden.";
-      }
-  } else {
-      echo "Fehler beim Ausführen der Abfrage: " . mysqli_error($conn);
-  }
-}
-// schluss der verbindung
-mysqli_close($conn);
+          ?>
+         <div class="book-details">
+  <div class="book-details-text-container">
+    <h2><?php echo $book_details['kurztitle']; ?></h2>
+    <p><strong>Titel:</strong> <?php echo $book_details['title']; ?></p>
+    <p><strong>Autor:</strong> <?php echo $book_details['autor']; ?></p>
+    <p><strong>Verfasser:</strong> <?php echo $book_details['verfasser']; ?></p>
+    <p><strong>Sprache:</strong> <?php echo $book_details['sprache']; ?></p>
+    <p><strong>Zustand:</strong> <?php echo $book_details['zustand']; ?></p>
 
-?>
-
-    </div>
+    <?php if (isset($book_details['text'])) { ?>
+      <p><strong>Inhalt:</strong></p>
+      <p><?php echo $book_details['text']; ?></p>
+    <?php } ?>
+  </div>
+  <div class="book-details-image">
+    <img src="<?php echo $random_cover; ?>" alt="Book cover">
+  </div>
 </div>
 
+        <?php
+        } else {
+          echo "Das Buch konnte nicht gefunden werden.";
+        }
+      } else {
+        echo "Fehler beim Ausführen der Abfrage: " . mysqli_error($conn);
+      }
+    }      
+// schluss der verbindung
+mysqli_close($conn);
+?>
+
+  </div>
+</div>
 <script>  
          //javascript für hamburger button 
         //der event listener wartet auf einen klick auf den hamburger button und führt dann die funktion aus die die klasse active hinzufügt
