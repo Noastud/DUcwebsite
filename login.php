8d7d965e1300c3c1604d160230f8e617e89f039a
Noastud <104830641+Noastud@users.noreply.github.com>
2023-05-21 15:55:50 +0200

    login immer no meh gmacht als julien

--- login.php
+++ login.php
@@ -6,13 +6,72 @@
     <title>Book Archive</title>
     <link rel="stylesheet" href="style/styles.css?v=2">
     <style>
-  .green-box {
-   color: var(--primary);
-    width: 200px;
-    height: 200px;
-    margin: 0 auto;
-    margin-top: 100px;
-}
+        .green-box {
+            background-color: #839788;
+            width: 1000px;
+            height: 550px;
+            margin: 0 auto;
+            margin-top: 50px;
+        }
+        .login-card {
+            width: 400px;
+            height: 550px;
+            background-color: #d9d9d9;
+            border-radius: 10px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            margin: 0 auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .login-card h2 {
+            text-align: center;
+            margin-top: 40px;
+            font-size: 54px;
+            color: #fff;
+            margin-bottom: 30px;
+        }
+
+        .login-card label {
+            display: block;
+            margin-bottom: 5px;
+            font-size: 14px;
+        }
+
+        .login-card input[type="text"],
+        .login-card input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 20px;
+            margin-bottom: 5px;
+            margin-left:15%;
+            margin-right:15%;
+            text-align: left;
+            background-color: #726C6C !important;
+        }
+
+        .login-card input[type="text"]:hover,
+        .login-card input[type="text"]:focus,
+        .login-card input[type="password"]:hover,
+        .login-card input[type="password"]:focus {
+            color: #000;
+            background-color: #fff !important;
+        }
+        .login-card button {
+            width: 30%;
+            padding: 10px;
+            background-color: #fff !important;
+            color: black;
+            margin: 0 auto;
+            border: none;
+            border-radius: 4px;
+            font-size: 30px;
+            cursor: pointer;
+        }
     </style>
     <script src="scripts.js" defer></script>
 </head>
@@ -24,7 +83,7 @@
     </div>
 </button>
 <nav>
-    <button class="btn" onclick="location.href='login.php'">Login</button>
+    <button class="btn" onclick="location.href='register.php'">Register</button>
     <hamburger-icon>
     <span></span>
     <span></span>
@@ -34,30 +93,25 @@
     <a href="admin.html" class="nav-link">Admin Panel</a>
     <a href="bookoverview.php" class="nav-link">Book Overview</a>
     <a href="details.php" class="nav-link">Search</a>    </div>
-</nav>
+    </nav>
 </header>
 
-<div class="green-box"></div>
-
-<div id="loginModal" class="modal">
-    <div class="modal-content">
-        <span class="close" id="closeBtn">&times;</span>
+<div class="green-box">
+    <div class="login-card">
         <h2>Login</h2>
         <form action="login.php" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            <br>
-            <input type="submit" value="Login">
+            <label for="username" class="input-label"></label>
+            <input type="text" id="username" name="username" required onfocus="if(this.value=='Username') this.value='';" onblur="if(this.value=='') this.value='Username';" value="Username">
+            <label for="password" class="input-label"></label>
+            <input type="password" id="password" name="password" required onfocus="if(this.value=='Password') this.value='';" onblur="if(this.value=='') this.value='Password';" value="Password">
+            <button type="submit">Submit</button>
         </form>
     </div>
 </div>
 
 <script>
     // Add this to your scripts.js file
-    const loginBtn = document.getElementById('loginBtn');
+    const loginBtn = document.querySelector('.btn');
     const closeBtn = document.getElementById('closeBtn');
     const loginModal = document.getElementById('loginModal');
 
@@ -74,6 +128,17 @@
             loginModal.style.display = 'none';
         }
     }
+              //javascript für hamburger button 
+          //der event listener wartet auf einen klick auf den hamburger button und führt dann die funktion aus die die klasse active hinzufügt
+          //die klasse active ist in der css datei definiert und sorgt dafür das die navigation angezeigt wird 
+          const hamburger = document.querySelector('hamburger-icon');
+        const nav = document.querySelector('header nav');
+        
+        hamburger.addEventListener('click', function() {
+            nav.classList.toggle('active');
+        });
+
 </script>
 </body>
 </html>
+
--- register.php
+++ register.php
@@ -3,53 +3,144 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book Archive - Register</title>
+    <title>Book Archive</title>
     <link rel="stylesheet" href="style/styles.css?v=2">
-   
+    <style>
+        .green-box {
+            background-color: #839788;
+            width: 1000px;
+            height: 550px;
+            margin: 0 auto;
+            margin-top: 50px;
+        }
+
+        .login-card {
+            width: 400px;
+            height: 550px;
+            background-color: #d9d9d9;
+            border-radius: 10px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            margin: 0 auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .login-card h2 {
+            text-align: center;
+            margin-top: 40px;
+            font-size: 54px;
+            color: #fff;
+            margin-bottom: 30px;
+        }
+
+        .login-card label {
+            display: block;
+            margin-bottom: 5px;
+            font-size: 14px;
+        }
+
+        .login-card input[type="text"],
+        .login-card input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 20px;
+            margin-bottom: 5px;
+            margin-left: 15%;
+            margin-right: 15%;
+            text-align: left;
+            background-color: #726C6C !important;
+        }
+
+        .login-card input[type="text"]:hover,
+        .login-card input[type="text"]:focus,
+        .login-card input[type="password"]:hover,
+        .login-card input[type="password"]:focus {
+            color: #000;
+            background-color: #fff !important;
+        }
+
+        .login-card button {
+            width: 30%;
+            padding: 10px;
+            background-color: #fff !important;
+            color: black;
+            margin: 0 auto;
+            border: none;
+            border-radius: 4px;
+            font-size: 30px;
+            cursor: pointer;
+        }
+    </style>
     <script src="scripts.js" defer></script>
 </head>
 <body>
 <header>
-    <h1>Book Archive</h1>
-
-    <nav>
-            <button class="btn" ><a href="login.php"> Login</a></button>
-        <hamburger-icon>
-            <span></span>
-            <span></span>
-            <span></span>
-        </hamburger-icon>
-        <div id="hamburger-menu" class="hidden">
-            <a href="admin.html" class="nav-link">Admin Panel</a>
-            <a href="bookoverview.php" class="nav-link">Book Overview</a>
-            <a href="register.html" class="nav-link">Register</a>
-        </div>
+<button onclick="location.href='index.php'" style="background:none; border:none; font-size: 30px;">
+    <div class="logo">
+    <h1>Bookly</h1>
+    </div>
+</button>
+<nav>
+    <button class="btn" onclick="location.href='login.php'">login</button>
+    <hamburger-icon>
+    <span></span>
+    <span></span>
+    <span></span>
+    </hamburger-icon>
+    <div id="hamburger-menu" class="hidden">
+    <a href="admin.html" class="nav-link">Admin Panel</a>
+    <a href="bookoverview.php" class="nav-link">Book Overview</a>
+    <a href="details.php" class="nav-link">Search</a>    </div>
     </nav>
 </header>
 
-<div class="main-content">
-    <div class="green-box">
-        <div class="modal-content">
-            <h2>Register</h2>
-            <form action="register.php" method="POST">
-                <label for="username"></label>
-                <input type="text" id="username" name="username" placeholder="Username" required>
-                <br>
-                <label for="email"></label>
-                <input type="email" id="email" name="email" placeholder="E-Mail" required>
-                <br>
-                <label for="password"></label>
-                <input type="password" id="password" name="password" placeholder="Password" required>
-                <br>
-                <input type="submit" value="Submit">
-            </form>
-        </div>
+<div class="green-box">
+    <div class="login-card">
+        <h2>Register</h2>
+        <form action="login.php" method="POST">
+            <label for="username" class="input-label"></label>
+            <input type="text" id="username" name="username" required onfocus="if(this.value=='Username') this.value='';" onblur="if(this.value=='') this.value='Username';" value="Username">
+            <label for="email" class="input-label"></label>
+            <input type="text" id="email" name="email" required onfocus="if(this.value=='E-mail') this.value='';" onblur="if(this.value=='') this.value='E-mail';" value="E-mail">
+            <label for="password" class="input-label"></label>
+            <input type="password" id="password" name="password" required onfocus="if(this.value=='Password') this.value='';" onblur="if(this.value=='') this.value='Password';" value="Password">
+            <button type="submit">Submit</button>
+        </form>
     </div>
 </div>
 
-<footer>
-    <p>&copy; 2023 Book Archive. All rights reserved.</p>
-</footer>
+<script>
+    // Add this to your scripts.js file
+    const loginBtn = document.querySelector('.btn');
+    const closeBtn = document.getElementById('closeBtn');
+    const loginModal = document.getElementById('loginModal');
 
+    loginBtn.onclick = () => {
+        loginModal.style.display = 'block';
+    }
+
+    closeBtn.onclick = () => {
+        loginModal.style.display = 'none';
+    }
+
+    window.onclick = (event) => {
+        if (event.target === loginModal) {
+            loginModal.style.display = 'none';
+        }
+    }
+    
+    // JavaScript for hamburger button
+    const hamburger = document.querySelector('hamburger-icon');
+    const nav = document.querySelector('header nav');
+    
+    hamburger.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
+</script>
 </body>
 </html>
+
